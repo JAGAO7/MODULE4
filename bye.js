c10974e40e2a2a3f90658fe5ca9881a2b9883018
JAGAO7 <65215925+JAGAO7@users.noreply.github.com>
2020-08-02 17:46:50 +0530

    Add files via upload

---
+++ bye.js
@@ -0,0 +1,14 @@
+(function (window){
+
+
+    byespeaker=new Object();
+
+    byespeaker.speak= function(name){
+
+        console.log("GOOD BYE "+ name);
+
+    }
+
+    window.byespeaker=byespeaker;
+
+})(window);
---
+++ hello.js
@@ -0,0 +1,16 @@
+(function (window){
+
+
+    var hellospeaker=new Object();
+
+    hellospeaker.speak= function(name)
+
+    {
+
+        console.log("Hello " + name);
+
+    }
+
+    window.hellospeaker=hellospeaker;
+
+})(window);
